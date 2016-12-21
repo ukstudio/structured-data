8b87c7e86aa434be81b9c452b9c35940276ad5de
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2016-12-21 16:45:23 +0900

    Initial implementation

--- lib/structured_data.rb
+++ lib/structured_data.rb
@@ -1,5 +1,138 @@
-require "structured_data/version"
+require 'action_controller'
+require 'action_view'
+require 'json'
 
 module StructuredData
-  # Your code goes here...
+  module ViewHelper
+    def repository
+      @structured_data_repository ||= StructuredData::Repository.new
+    end
+
+    def breadcrumb_list
+      @breadcrumb_list ||= StructuredData::BreadcrumbList.new
+    end
+
+    def set_breadcrumb_item(url:, name:)
+      breadcrumb_list << { url: url, name: name }
+    end
+
+    def site_navigation_element
+      @site_navigation_element ||= StructuredData::SiteNavigationElement.new
+    end
+
+    def set_site_navigation_element(url:, name:)
+      site_navigation_element << { url: url, name: name }
+    end
+
+    def display_strctured_data
+      repository << breadcrumb_list unless breadcrumb_list.empty?
+      repository << site_navigation_element unless site_navigation_element.empty?
+
+      self.content_tag(:script, repository.dump.html_safe, type: 'application/ld+json')
+    end
+  end
+
+  module ControllerHelper
+    def breadcrumb_list
+      @breadcrumb_list ||= StructuredData::BreadcrumbList.new
+    end
+
+    def set_breadcrumb_item(url:, name:)
+      breadcrumb_list << { url: url, name: name }
+    end
+
+    def site_navigation_element
+      @site_navigation_element ||= StructuredData::SiteNavigationElement.new
+    end
+
+    def set_site_navigation_element(url:, name:)
+      site_navigation_element << { url: url, name: name }
+    end
+  end
+
+  class Repository
+    def initialize
+      @items = []
+    end
+
+    def <<(item)
+      @items << item
+    end
+
+    def dump
+      "[" + @items.map(&:dump).join(',') + "]"
+    end
+  end
+
+  class BreadcrumbList
+    def initialize
+      @items = []
+    end
+
+    def <<(item)
+      @items << item
+    end
+
+    def empty?
+      @items.empty?
+    end
+
+    def dump
+      hash = {
+        "@context": "http://schema.org",
+        "@type": "BreadcrumbList",
+      }
+
+      hash['itemListElement'] = items_to_hash
+      JSON.pretty_generate(hash)
+    end
+
+    private
+
+    def items_to_hash
+      @items.map.with_index do |item, i|
+        {
+          "@type": "ListItem",
+          "position": i + 1,
+          "item": {
+            "@id": item[:url],
+            "name": item[:name]
+          }
+        }
+      end
+    end
+  end
+
+  class SiteNavigationElement
+    def initialize
+      @urls  = []
+      @names = []
+    end
+
+    def <<(item)
+      @urls  << item[:url]
+      @names << item[:name]
+
+      item
+    end
+
+    def empty?
+      @urls.empty? && @names.empty?
+    end
+
+    def dump
+      hash = {
+        "@context": "http://schema.org",
+        "@type": "SiteNavigationElement",
+      }
+
+      hash[:url] = @urls
+      hash[:name] = @names
+
+      JSON.pretty_generate(hash)
+    end
+  end
 end
+
+ActionView::Base.send :include, StructuredData::ViewHelper
+ActionController::Base.send :include, StructuredData::ControllerHelper
--- spec/structured_data_spec.rb
+++ spec/structured_data_spec.rb
@@ -1,11 +1,51 @@
-require "spec_helper"
+require 'spec_helper'
+require 'json'
 
-describe StructuredData do
-  it "has a version number" do
-    expect(StructuredData::VERSION).not_to be nil
+RSpec.describe StructuredData::Repository do
+  specify do
+    repo = described_class.new
+
+    list = StructuredData::BreadcrumbList.new
+    list << { url: '/products', name: 'Products' }
+    list << { url: '/products/book', name: 'Book' }
+
+    repo << list
+
+    element = StructuredData::SiteNavigationElement.new
+    element << { url: '/products', name: 'Products' }
+    element << { url: '/FAQ', name: 'FAQ' }
+
+    repo << element
+
+    parsed = JSON.parse(repo.dump)
+
+    expect(parsed.size).to eq 2
   end
+end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe StructuredData::BreadcrumbList do
+  specify do
+    list = described_class.new
+    list << { url: '/products', name: 'Products' }
+    list << { url: '/products/book', name: 'Book' }
+
+    parsed = JSON.parse(list.dump)
+
+    expect(parsed['@context']).to eq 'http://schema.org'
+    expect(parsed['itemListElement'][0]['item']['@id']).to eq '/products'
+ end
+end
+
+RSpec.describe StructuredData::SiteNavigationElement do
+  specify do
+    element = described_class.new
+    element << { url: '/products', name: 'Products' }
+    element << { url: '/FAQ', name: 'FAQ' }
+
+    parsed = JSON.parse(element.dump)
+
+    expect(parsed['@context']).to eq 'http://schema.org'
+    expect(parsed['url'][0]).to eq "/products"
+    expect(parsed['name'][0]).to eq "Products"
   end
 end
